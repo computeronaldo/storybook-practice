5d18412d6dadaec325ab1befadf5a1cedcc133f7
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2024-12-06 17:02:13 +0530

    first commit

---
+++ src/stories/Tablr.stories.js
@@ -0,0 +1,22 @@
+import { Tablr } from "../Tablr";
+
+export default {
+  title: "Tablr - A Customizable React Table Component",
+  component: Tablr,
+  argTypes: {
+    rows: { control: "" },
+    headers: { control: "" },
+  },
+};
+
+export const Default = {
+  args: {
+    rows: [
+      ["This", "is", "just", "a", "test"],
+      ["This", "is", "also", "a", "test"],
+      ["Just", "a", "little", "more", "data"],
+      ["Row", "number", "four", "right", "here"],
+    ],
+    headers: ["Col 1", "Col 2", "Col 3", "Col 4", "Col 5"],
+  },
+};
